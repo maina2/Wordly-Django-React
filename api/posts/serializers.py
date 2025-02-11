34f56f2a68595f073cb01d65f04a5a4c8cde1046
Simon Chanzu <chanzusimon6@gmail.com>
2025-02-11 20:42:14 +0300

    uploads today

---
+++ api/posts/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Post
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ('id', 'title', 'content', 'image', 'created_at', 'updated_at')  # Exclude 'author'
+        read_only_fields = ('id', 'created_at', 'updated_at')
--- api/wordlyBackend/settings.py
+++ api/wordlyBackend/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
     'users',
+    'posts',
+    'comments',
 ]
 
 REST_FRAMEWORK = {
@@ -55,8 +57,8 @@ REST_FRAMEWORK = {
     ),
 }
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=300),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=10),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
     "AUTH_HEADER_TYPES": ("Bearer",),
